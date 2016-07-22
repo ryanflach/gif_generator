9a15c57535fd65352b0cf7da368521a508952c58
Ryan Flach <ryanflach@users.noreply.github.com>
2016-07-22 11:56:03 -0600

    [WIP] Add enum to role column of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   validates :email, presence: true, uniqueness: true
+
+  enum role: [:default, :admin]
 end
